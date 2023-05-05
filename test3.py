51093f33e3e9b6e8af21ff020e3bf6ecffdc4cda
nick0413 <68749910+nick0413@users.noreply.github.com>
2023-05-05 11:46:47 -0500

    Avance en traduccion
    La traduccion es casi perfecta pero para relaciones que involucran elementos del estilo 'Factor-H' aun no funciona. Parece que 'Factor-H'  no esta en la lista de elementos C

--- test3.py
+++ test3.py
@@ -8,16 +8,154 @@ import re
 import openai as gpt
 
 
-def chachito(pregunta,info=''):
+# def chachito(pregunta,info=''):
 
-	gpt.api_key="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"
-	conectado=pregunta+info
-	resultado=gpt.Completion.create(engine="text-davinci-003",prompt=conectado,n=1,max_tokens=4000)
-	print(resultado.choices[0].text)
+# 	gpt.api_key="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"
+# 	conectado=pregunta+info
+# 	resultado=gpt.Completion.create(engine="text-davinci-003",prompt=conectado,n=1,max_tokens=4000)
+# 	print(resultado.choices[0].text)
 
+def cargar_interacciones():
+	my_file = open("tipos_interaccion.txt", "r")
+
+	data = my_file.read()
+
+	data_into_list = data.split("\n")
+	my_file.close()
+
+	return data_into_list
+def print_datos(R,C,tipo):
+	if tipo=="C":
+		for c in C:
+			print(c)
+	if tipo=='R':
+		for r in R:
+			print(r)
+
+def uniones(frase,interacciones, elementos):
+	lista_listas=[]
+	palabras=frase.split(" ")
+
+	found_start=False
+	start=None
+	targets=[]
+
+	for palabra in palabras:
+		#print(palabra)
+		if palabra in interacciones:
+			tipo_interaccion=interacciones.index(palabra)
+
+		if palabra in elementos:
+			if not found_start:
+				start=palabra
+				found_start=True
+			else:
+				targets.append(palabra)
+
+	for target in targets:
+		conexion=[start,target]
+		lista_listas.append(conexion)
+
+	return lista_listas
+
+def uniones_2(frase,interacciones, elementos):
+	lista_listas=[]
+	inter=None
+	found_start=False
+	istart=None
+	start=None
+	targets=[]
+	print(frase)
+
+	for elemento in elementos:		#revisa todos los elementos conocidos
+		found=frase.find(elemento)	#si no esta en la frase, pasa a la siguiente
+
+		if found==-1:
+			pass
+
+		if not found_start: 				#Si no ha encontrado el nodo incial
+			if frase.find(elemento)!=-1: 	#si este elemento esta en la frase
+				start=elemento				#entonces este elemento debe ser el nodo inicial
+				found_start=True			
+				print('found',elemento)
+
+
+		if found_start:										#si ya encontramos el inicial
+			if frase.find(elemento)<frase.find(start):		#y encontramos un elemento que esta mas a la izquierda 
+				if frase.find(elemento)!=-1:				#en la frase
+					start=elemento							#entonces ese debe ser el inicial
+
+			if elemento.find(start)>-1:						#si hay un elemento mas complejo
+				if (frase.find(elemento))!=-1:				#que tambien esta en la frase
+					start=elemento							#entonces el inicial debe ser la forma compleja
+
+			if (elemento.find(start))==-1:					#si el elemento no es el inicial
+				#print(elemento,start,elemento.find(start))
+				if (frase.find(elemento))!=-1:				#pero si esta en la frase
+					print(elemento,start,elemento.find(start))
+					if (start.find(elemento))==-1:			#y no es una version mas simple del inicial
+						if not (elemento in targets):
+							targets.append(elemento)
+									
+
+	for interaccion in interacciones:
+		found=frase.find(elemento)
+		if found==-1:
+			pass
+		else:
+			inter=interaccion
+
+	for target in targets:
+		conexion=[start,target]
+		lista_listas.append(conexion)
+		print(conexion)
+
+	
+	#print(len(targets))
+
+	return lista_listas
+
+def uniones_3(frase,interacciones, elementos):
+	lista_listas=[]
+	matches=[]
+	matches_index=[]
+	targets=[]
+
+	found_start=False
+	print('----------')
+	print(frase)
+
+	for elemento in elementos:
+		#print(elemento,frase.find(elemento))
+		if frase.find(elemento)!=-1:
+			print('found')
+			if elemento not in matches:
+				matches.append(elemento)
+				matches_index.append(frase.find(elemento))
+	print(matches)
+	sorted_matches = [x for _,x in sorted(zip(matches_index,matches))]
+
+
+	if len(matches)==1:
+		lista_listas=[matches[0],matches[0]]
+		return lista_listas
+	else:
+		for element in matches:
+			if not found_start:
+				start=element
+				found_start=True
+			else:
+				targets.append(element)
+	for target in targets:
+		lista_listas.append([start,target])
+		print([start,target])
+
+	
+	return lista_listas
 
 
 # ? Este bloque trae el archivo sbml como un modelo de libsbml
+
 sbml_file = "Datos.sbml"
 reader = libsbml.SBMLReader()
 document = reader.readSBMLFromFile(sbml_file)
@@ -43,31 +181,59 @@ for comp in range(model.getNumSpecies()):#Este ciclo trae las componentes quimic
 	C.append((componente.getName()))
 
 
-for i in R:
-	print(i)
 C_elementos=[]
 
 '''
 Los componentes en el sistema de especies de SBML no estan estructurados correctamente 
 para la interpretacion de un grafo. Este bloque de codigo los reestructura apropiadamente.
 '''
+for r in R:
+	print(r)
 for i in C:
-	C_n=i.split(",")
-	for componente in C_n:
-		#print(componente)
-		componente=re.sub("[\(\[].*?[\)\]]", "", componente)
-		componente=componente.strip()
-		componente=re.sub(" ","-",componente)
-		#print(componente)
-		if componente != '':
-			C_elementos.append(componente)
+
+	C_n1=i.split(",")
+	
+	for C_n2 in C_n1:
+		C_n=C_n2.split(":")
+		for componente in C_n:
+			#print(componente)
+			componente=re.sub("[\(\[].*?[\)\]]", "", componente)#esto borra lo que esta dentro de corchetes
+			componente=componente.strip()
+			#componente=re.sub(" ","-",componente)
+			#print(componente)
+			if componente != '':
+				C_elementos.append(componente)
 
 
+C2=[]
+R2=[]
+for c in C_elementos:
+	c2=re.sub(" ","-",c)
+	c2='-'+c2+'-'
+
+	C2.append(c2)
 
 
-# for c in C_elementos:
-# 	print(c)
+for r in R:
+	r=r.rstrip()
+	r=re.sub(",","",r)
+	r=re.sub(" ","-",r)
+	R2.append('-'+r+'-')
 
+
+interacciones=cargar_interacciones()
+
+print_datos(R2,C2,'')
+
+t=0
+# for r in R2:
+# 	h=uniones_3(r,interacciones,C2)
+# 	t=t+1
+# 	print(t)
+# 	if t>10:
+# 		break
+
+uniones_3(R2[9],interacciones,C2)
 	
 
 
